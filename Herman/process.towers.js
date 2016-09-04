dc25358a9514d5d6093d0ab9bc4f3a00d57c41f5
HJCPeacock <hjcpeacock@gmail.com>
2016-09-04 19:02:15 +0200

    towers

--- Herman/process.towers.js
+++ Herman/process.towers.js
@@ -9,10 +9,10 @@ var processTowers = {
             
             var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return ((structure.structureType == STRUCTURE_WALL && structure.hits < 2000) ||
+                    return ((structure.structureType == STRUCTURE_WALL && structure.hits < 100000) ||
                                 (structure.structureType == STRUCTURE_SPAWN && structure.hits < structure.hitsMax) ||
                                 (structure.structureType == STRUCTURE_TOWER && structure.hits < structure.hitsMax) ||
-                                (structure.structureType == STRUCTURE_RAMPART && structure.hits < 40000) ||
+                                (structure.structureType == STRUCTURE_RAMPART && structure.hits < 45000) ||
                                 (structure.structureType == STRUCTURE_ROAD && structure.hits < structure.hitsMax)
                                 );
                 }
