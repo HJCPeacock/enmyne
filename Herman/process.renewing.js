ff19eee124ff4d2badd794bcb20a25faa87cb22d
HJCPeacock <hjcpeacock@gmail.com>
2016-09-05 21:53:33 +0200

    ..

--- Herman/process.renewing.js
+++ Herman/process.renewing.js
@@ -2,8 +2,15 @@ var processRenewing = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-
-        
+        var spawn = Game.spawns['Spawn1'];
+        var renewCounter = 3;
+        if (spawn.renewCreep(creep) == ERR_NOT_IN_RANGE) creep.moveTo(spawn);
+        else if (spawn.renewCreep(creep) == OK) {
+            for (var x = 0; x < 3; x++) {
+                if (spawn.renewCreep(creep) == OK) break;
+            }
+            creep.memory.renewed = true;
+        }
     }
 };
 
