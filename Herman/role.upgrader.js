00aefb397ed5dff4aff1dbb9ede2bfba0bfab814
HJCPeacock <hjcpeacock@gmail.com>
2016-09-05 22:37:19 +0200

    added reuse path

--- Herman/role.upgrader.js
+++ Herman/role.upgrader.js
@@ -16,12 +16,12 @@ var roleUpgrader = {
 	    if(!creep.memory.upgrading) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                creep.moveTo(sources[0], {reusePath:18});
             }
         }
         else {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+                creep.moveTo(creep.room.controller, { reusePath: 18 });
             }
         }
 	}
