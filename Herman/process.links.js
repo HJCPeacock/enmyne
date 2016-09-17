1bff9aa49c3ee9a959025db4012b917cb3428a5c
HJCPeacock <hjcpeacock@gmail.com>
2016-09-17 17:08:45 +0200

    Added process links

---
+++ Herman/process.links.js
@@ -0,0 +1,20 @@
+﻿var processLinks = {
+    run: function () {
+
+        //room W59S26
+        //at source 1
+        var linkFrom1 = Game.spawns.Spawn1.room.lookForAt('structure', 20, 33)[0];
+        var linkTo = Game.spawns.Spawn1.room.lookForAt('structure', 36, 24)[0];
+
+        if (linkFrom1.cooldown == 0 && linkTo.energyCapacity - linkTo.energy > linkFrom1.energy && linkFrom1.energy > 200)
+            linkFrom1.transferEnergy(linkTo);
+
+        //at source 2
+        var linkFrom2 = Game.spawns.Spawn1.room.lookForAt('structure', 22, 42)[0];
+
+        if (linkFrom2.cooldown == 0 && linkTo.energyCapacity - linkTo.energy > linkFrom2.energy && linkFrom2.energy > 200)
+            linkFrom2.transferEnergy(linkTo);
+    }
+};
+
+module.exports = processLinks;
