efb8009d7583125101a75088d6d95f096c682571
HJCPeacock <hjcpeacock@gmail.com>
2016-09-18 19:52:35 +0200

    prioritirize ramparts

--- Herman/role.repairer.js
+++ Herman/role.repairer.js
@@ -2,7 +2,11 @@ var roleRepair = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
+        var ramparts = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_RAMPART && structure.hits < 200000);
+            }
+        });
 	    var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return ((structure.structureType == STRUCTURE_SPAWN && structure.hits < structure.hitsMax) ||
@@ -12,9 +16,14 @@ var roleRepair = {
                             (structure.structureType == STRUCTURE_ROAD && structure.hits < structure.hitsMax)
                             );
                 }
-        });
-            
-        if(targets.length > 0) {
+	    });
+	    if (ramparts.length > 0)
+	    {
+	        if (creep.repair(ramparts[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+	            creep.moveTo(ramparts[0]);
+	        }
+	    }
+        else if(targets.length > 0) {
             if(creep.repair(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(targets[0]);
             }
