7ab76542ec1f74c4458bf4ba64a66a920f8910b2
HJCPeacock <hjcpeacock@gmail.com>
2016-09-18 18:03:41 +0200

    attacker now heals self

--- Herman/process.spawning.js
+++ Herman/process.spawning.js
@@ -40,7 +40,7 @@ var processSpawning = {
         var carrierBody = [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]; //550 | 0.5 * 10 / 5 = 1  | 10 * 1 - 2 * 5 = 10 - 10 = 0
         var builderBody = [WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE];//950 | 1 * 8 / 8 = 1 | 8 * 2 - 2 * 8 = 0
         var upgraderBody = [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE]; //950 | 0.5 * 10 / 5 = 1  | 10 * 1 - 2 * 5 = 10 - 10 = 0
-        var attackerBody = [WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,RANGED_ATTACK];
+        var attackerBody = [MOVE, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, HEAL];
     
         if (harvesters.length < hervesterlimit && spawn.canCreateCreep(harvesterBody, undefined) == OK) {
             var newName = spawn.createCreep(harvesterBody, undefined, {role: 'harvester', harvesting: true, source: getSourceCount() == 0 ? 0 : 1});
--- Herman/role.attack.js
+++ Herman/role.attack.js
@@ -1,13 +1,10 @@
-var roleRepairer = require('role.repairer');
-
 var roleAttack = {
-    run: function(creep) {
+    run: function (creep) {
+        if (creep.hits < creep.hitsMax) creep.heal(creep);
         var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 22);
         if(targets.length > 0) {
             if (creep.rangedAttack(targets[0]) == ERR_NOT_IN_RANGE) creep.moveTo(targets[0]);
             creep.say('Attacking!');
-        } else {
-            roleRepairer.run(creep);
         }
 	}
 };
