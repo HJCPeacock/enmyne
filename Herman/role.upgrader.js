5f49bd0112d68674a1c652c857911d8c380927bf
HJCPeacock <hjcpeacock@gmail.com>
2016-09-18 17:54:16 +0200

    ..

--- Herman/role.upgrader.js
+++ Herman/role.upgrader.js
@@ -5,12 +5,10 @@ var roleUpgrader = {
        
          if(creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
-            creep.say('harvesting');
 	    }
 	    
 	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.upgrading = true;
-	        creep.say('upgrading');
 	    }
         
 	    if(!creep.memory.upgrading) {
