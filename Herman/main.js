8fd4c2bd727870c2eff005e1474b59082f66f621
HJCPeacock <hjcpeacock@gmail.com>
2016-09-25 19:49:24 +0200

    ..

--- Herman/main.js
+++ Herman/main.js
@@ -55,7 +55,7 @@ module.exports.loop = function () {
     }
 
     function calculateUpgraderCount() {
-        if (!Game.rooms['E51N1'].storage) return 1;
+        if (!Game.rooms['E51N1'].storage) return 3;
         return Math.ceil(Game.rooms['E51N1'].storage.store[RESOURCE_ENERGY] / 10000);
     }
 
