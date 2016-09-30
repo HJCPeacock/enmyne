b5bf6f9b0076f8e12458805437c38be155480d7d
HJCPeacock <hjcpeacock@gmail.com>
2016-10-01 01:20:52 +0200

    ..

--- Herman/role.claim.js
+++ Herman/role.claim.js
@@ -1,3 +1,6 @@
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
 var rolePath = {
 
     /** @param {Creep} creep **/
@@ -9,35 +12,11 @@ var rolePath = {
         }
         else {
            
-            if (creep.memory.upgrading && creep.carry.energy == 0) {
-                creep.memory.upgrading = false;
+            if (creep.memory.role == 'upgrader') {
+                roleUpgrader.run(creep);
             }
-
-            if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-                creep.memory.upgrading = true;
-            }
-
-            if (!creep.memory.upgrading) {
-                var sourceStorage = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return structure.structureType == STRUCTURE_STORAGE;
-                    }
-                });
-                if (sourceStorage) {
-                    if (creep.withdraw(sourceStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(sourceStorage);
-                    }
-                } else {
-                    var sources = creep.room.find(FIND_SOURCES);
-                    if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(sources[0]);
-                    }
-                }
-            }
-            else {
-                if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller);
-                }
+            if (creep.memory.role == 'builder') {
+                roleBuilder.run(creep);
             }
         }
     }
