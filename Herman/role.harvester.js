301a92a4a87a133f021fbdbd4841af7cef5fe3b5
HJCPeacock <hjcpeacock@gmail.com>
2016-10-01 23:50:25 +0200

    ..

--- Herman/role.harvester.js
+++ Herman/role.harvester.js
@@ -36,8 +36,9 @@ var roleHarvester = {
                 var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                                    structure.structureType == STRUCTURE_SPAWN ||
+                                    structure.structureType == STRUCTURE_TOWER ||
+                                    structure.structureType == STRUCTURE_STORAGE) && structure.energy < structure.energyCapacity;
                     }
                 });
                 if(target) {
