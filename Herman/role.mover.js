dfaffaa7f470c4fa518c972e841701a13affded9
HJCPeacock <hjcpeacock@gmail.com>
2016-10-02 15:05:54 +0200

    ..

--- Herman/role.mover.js
+++ Herman/role.mover.js
@@ -23,30 +23,30 @@ var roleMover = {
             if (creep.room.name != 'E51N1') {
                 var exitDir = Game.map.findExit(creep.room.name, 'E51N1');
                 var Exit = creep.pos.findClosestByRange(exitDir);
-                creep.moveTo(Exit);
+               
 
                 var found = creep.pos.lookFor(LOOK_STRUCTURES);
-                if (found.length == 0) {
-                    creep.room.createConstructionSite(creep.pos, STRUCTURE_ROAD);
-                }
-
                 var building = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-                if (building) {
-                    if (creep.build(building) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(building);
-                    }
-                }
-
                 var repairUnit = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return structure.structureType == STRUCTURE_ROAD && structure.hits < structure.hitsMax;
                     }
                 });
-                if (repairUnit)
+
+                if (found.length == 0) {
+                    creep.room.createConstructionSite(creep.pos, STRUCTURE_ROAD);
+                }
+                else if (building) {
+                    if (creep.build(building) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(building);
+                    }
+                }
+                else if (repairUnit)
                 {
                     if (creep.repair(repairUnit) == ERR_NOT_IN_RANGE);
                     creep.moveTo(repairUnit);
                 }
+                else creep.moveTo(Exit);
             } else {
                 var storage = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) => {
