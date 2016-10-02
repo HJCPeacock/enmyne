d74f8d63b6a6e21cca895d2d0df12228217b0b7c
HJCPeacock <hjcpeacock@gmail.com>
2016-10-02 09:48:45 +0200

    ..

--- Herman/role.claim.js
+++ Herman/role.claim.js
@@ -5,8 +5,8 @@ var rolePath = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        if (creep.room.name != 'E51N3') {
-            var exitDir = Game.map.findExit(creep.room.name, 'E51N3');
+        if (creep.room.name != 'E51N2') {
+            var exitDir = Game.map.findExit(creep.room.name, 'E51N2');
             var Exit = creep.pos.findClosestByRange(exitDir);
             creep.moveTo(Exit);
         }
