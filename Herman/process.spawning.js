138a74adb22fd1a789289884f229202b0857c50d
HJCPeacock <hjcpeacock@gmail.com>
2016-10-04 18:00:30 +0200

    ..

--- Herman/process.spawning.js
+++ Herman/process.spawning.js
@@ -99,7 +99,7 @@ var processSpawning = {
                 }
                 return;
             }
-            else if (movers.length < 1 && room.name == 'E51N1') {
+            else if (movers.length < 2 && room.name == 'E51N1') {
                 if (spawn.canCreateCreep(moverBody, undefined) == OK) {
                     var newName = spawn.createCreep(moverBody, undefined, { role: 'mover', room: setMoverRoom() });
                     console.log('Spawning new mover: ' + newName);
@@ -120,7 +120,7 @@ var processSpawning = {
         }
 
         function setMoverRoom() {
-            if (_.filter(movers, (creep) => creep.memory.room == 'E51N2').length == 0) return 'E51N2';
+            if (_.filter(movers, (creep) => creep.memory.room == 'E51N2').length < 2) return 'E51N2';
         }
 
         function setClaimRoom() {
