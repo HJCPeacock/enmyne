7af8e73b211c16d28a575b092feefbb7b744c2be
HJCPeacock <hjcpeacock@gmail.com>
2016-10-15 23:43:21 +0200

    Improved links ; added room 2 links

--- Herman/process.links.js
+++ Herman/process.links.js
@@ -2,25 +2,42 @@ var processLinks = {
     run: function (room) {
 
         //room E51N1
-        if (room.name != 'E51N1') return;
-        if (!Game.spawns.Spawn1) return;
+        if (room.name == 'E51N1') {
+            if (!Game.spawns.Spawn1) return;
 
-        //at source 1
-        var linkFrom1 = Game.spawns.Spawn1.room.lookForAt('structure', 5, 11)[0];
-        if (linkFrom1 == undefined) return;
+            var linkFrom1 = Game.spawns.Spawn1.room.lookForAt('structure', 5, 11)[0];
+            var linkTo = Game.spawns.Spawn1.room.lookForAt('structure', 17, 23)[0];
+            var linkFrom2 = Game.spawns.Spawn1.room.lookForAt('structure', 36, 16)[0];
 
-        var linkTo = Game.spawns.Spawn1.room.lookForAt('structure', 17, 23)[0];
-        if (linkTo == undefined) return;
+            //at source 1
+            if (linkFrom1 != undefined && linkTo != undefined)
+            {
+                if (linkFrom1 && linkFrom1.cooldown == 0 && linkTo.energyCapacity - linkTo.energy >= linkFrom1.energy && linkFrom1.energy >= 200)
+                    linkFrom1.transferEnergy(linkTo);
+            }
 
-        if (linkFrom1 && linkFrom1.cooldown == 0 && linkTo.energyCapacity - linkTo.energy >= linkFrom1.energy && linkFrom1.energy >= 200)
-            linkFrom1.transferEnergy(linkTo);
+            //at source 2
+            if (linkFrom2 != undefined && linkTo != undefined)
+            {
+                if (linkFrom2 && linkFrom2.cooldown == 0 && linkTo.energyCapacity - linkTo.energy >= linkFrom2.energy && linkFrom2.energy >= 200)
+                    linkFrom2.transferEnergy(linkTo);
+            }
+        }
 
-        //at source 2
-        var linkFrom2 = Game.spawns.Spawn1.room.lookForAt('structure', 36, 16)[0];
-        if (linkFrom2 == undefined) return;
+        //Room E51N3
+        if (room.name == 'E51N3') {
+            if (!Game.spawns.Spawn2) return;
 
-        if (linkFrom2 && linkFrom2.cooldown == 0 && linkTo.energyCapacity - linkTo.energy >= linkFrom2.energy && linkFrom2.energy >= 200)
-            linkFrom2.transferEnergy(linkTo);
+            var linkFrom1 = Game.spawns.Spawn2.room.lookForAt('structure', 6, 10)[0];
+            var linkTo = Game.spawns.Spawn2.room.lookForAt('structure', 26, 37)[0];
+
+            //at source 1
+            if (linkFrom1 != undefined && linkTo != undefined)
+            {
+                if (linkFrom1 && linkFrom1.cooldown == 0 && linkTo.energyCapacity - linkTo.energy >= linkFrom1.energy && linkFrom1.energy >= 200)
+                    linkFrom1.transferEnergy(linkTo);
+            }
+        }
     }
 };
 
