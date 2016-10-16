e2c89c16a64d8f7269b3b9475af238067d9a308a
HJCPeacock <hjcpeacock@gmail.com>
2016-10-16 18:06:04 +0200

    ..

--- Herman/role.extractor.js
+++ Herman/role.extractor.js
@@ -1,6 +1,41 @@
 var roleExtractor = {
     run: function (creep) {
-        //to do
+        //store[RESOURCE_ENERGY]
+        if (!creep.memory.harvesting && creep.carry.energy == 0) {
+            creep.memory.harvesting = true;
+        }
+        if (creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.harvesting = false;
+        }
+
+        if (creep.memory.harvesting) {
+            var targets = creep.pos.findInRange(FIND_DROPPED_ENERGY, 3);
+            if (targets.length > 0) {
+                if (creep.pickup(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+            else {
+                var source =  creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return structure.structureType == STRUCTURE_EXTRACTOR;
+                    }
+                });
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) creep.moveTo(source);
+            }
+        }
+        else {
+            var terminal = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType == STRUCTURE_TERMINAL;
+                }
+            });
+            if (terminal && terminal) {
+                if (creep.transfer(terminal, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(terminal);
+                }
+            }
+        }
     }
 };
 
