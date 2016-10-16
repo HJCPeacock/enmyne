f2fb81b359ff6a1931f178995e3ef8a040596bac
HJCPeacock <hjcpeacock@gmail.com>
2016-10-16 12:00:30 +0200

    role.attack added canHeal check

--- Herman/role.attack.js
+++ Herman/role.attack.js
@@ -1,10 +1,11 @@
 var roleAttack = {
     run: function (creep) {
-        if (creep.hits < creep.hitsMax) creep.heal(creep);
+        var canHeal = _.filter(creep.body, (x) => x.type == 'heal').length > 0;
+        if (canHeal && creep.hits < creep.hitsMax) creep.heal(creep);
+
         var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 22);
         if(targets.length > 0) {
             if (creep.rangedAttack(targets[0]) == ERR_NOT_IN_RANGE) creep.moveTo(targets[0]);
-            creep.say('Attacking!');
         }
 	}
 };
