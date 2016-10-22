6a2705ce2c9f14b41cde137a639c63255022d61f
HJCPeacock <hjcpeacock@gmail.com>
2016-10-22 21:04:47 +0200

    ..

--- Herman/role.repairer.js
+++ Herman/role.repairer.js
@@ -29,7 +29,7 @@ var roleRepair = {
                     }
                 }
 
-                if (lowestHP.hp >= highestHP.hp - 500) Memory.RoomsWallRampart[room].wallRampartHP = highestHP.hp + 50000;
+                if (lowestHP.hp >= highestHP.hp - 1000) Memory.RoomsWallRampart[room].wallRampartHP = highestHP.hp + 50000;
             }
 
             //Clearing Memory
