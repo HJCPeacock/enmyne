7341f55df770d488c3122cfed60672109d3cea02
HJCPeacock <hjcpeacock@gmail.com>
2016-10-30 02:04:43 +0200

    ..

--- Herman/process.terminal.js
+++ Herman/process.terminal.js
@@ -10,7 +10,7 @@ var processTerminal = {
             if (room.terminal.store[RESOURCE_ENERGY] >= 10000)
             {
                 var orders = Game.market.getAllOrders({ type: ORDER_BUY, resourceType: RESOURCE_CATALYST });
-                var feasibleOrders = _.filter(orders, (x) => Game.market.calcTransactionCost(x.amount, room.name, x.roomName) <= 10000 && x.price > 0.5);
+                var feasibleOrders = _.filter(orders, (x) => Game.market.calcTransactionCost(x.amount, room.name, x.roomName) <= 10000 && x.price >= 0.5);
 
                 if (feasibleOrders.length > 0) feasibleOrders.forEach(feasibleOrder => marketDeal(feasibleOrder));
             }
@@ -18,7 +18,7 @@ var processTerminal = {
 
         function marketDeal(order)
         {
-            //if (room.terminal.store[RESOURCE_CATALYST] > order.amount) Game.market.deal(order.id, order.amount, room.name);
+            if (room.terminal.store[RESOURCE_CATALYST] >= order.amount) Game.market.deal(order.id, order.amount, room.name);
         }
     }
 };
